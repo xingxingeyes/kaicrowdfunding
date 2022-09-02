fdf53a1662ae829f867c2248810691af8d0f0fbc
kai.lv_work <xingxingeyes@163.com>
2022-09-02 17:32:29 +0800

    权限控制

--- src/main/java/com/kai/crowd/constant/CrowdConstant.java
+++ src/main/java/com/kai/crowd/constant/CrowdConstant.java
@@ -7,6 +7,7 @@ public class CrowdConstant {
     public static final String MESSAGE_ACCESS_FORBIDDEN = "请登录后在访问！";
     public static final String MESSAGE_STRING_INVALIDATE = "字符串不合法！请不要传入空字符串！";
     public static final String MESSAGE_SYSTEM_ERROR_LOGIN_NOT_UNIQU = "系统错误：登录账号不唯一";
+    public static final String MESSAGE_ACCESS_DEIED = "抱歉，你不能访问这个资源！";
 
     public static final String ATTR_NAME_EXCEPTION = "exception";
     public static final String ATTR_NAME_ADMIN = "loginAdmin";
